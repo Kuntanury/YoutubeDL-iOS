3dcb312262fcc695082a2044b0fbc37a6abeea0b
Dextran <Kuntanury@users.noreply.github.com>
2025-01-12 22:42:02 +0800

    Update YoutubeDL.swift

--- Sources/YoutubeDL/YoutubeDL.swift
+++ Sources/YoutubeDL/YoutubeDL.swift
@@ -280,8 +280,8 @@ open class YoutubeDL: NSObject {
     }
     
     func loadPythonModule(downloadPythonModule: Bool = true) async throws -> PythonObject {
+        PythonSupport.initialize()
         if Py_IsInitialized() == 0 {
-            PythonSupport.initialize()
         }
         
         if !FileManager.default.fileExists(atPath: Self.pythonModuleURL.path) {
